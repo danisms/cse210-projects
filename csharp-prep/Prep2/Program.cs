27fc893b2dabb5c117a5683ccf57fd1ba4a5f3a9
Daniel C. Opute <danielcopute@gmail.com>
2024-04-25 19:22:19 +0100

    Completed Prep2 (Core-Requirements and Stretch Challenge)

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,82 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Welcome user
+        Console.WriteLine("Hello! Welcome to Prep2!");
+
+        // define important variables
+        int userGradeNum = 0;
+        string letter;
+        string sign;
+        bool digit = false;
+
+        // get grade value from user (check for value error and correct it)
+        while (!digit)
+        {
+            try
+            {
+                Console.Write("Enter your grade percentage: ");
+                userGradeNum = int.Parse(Console.ReadLine());
+                digit = true;
+            }
+            catch (Exception errorMsg)
+            {
+                Console.WriteLine(errorMsg);
+                Console.WriteLine("Invalid Entry. Numbers only (Do not include the % sign)");
+                digit = false;
+            }
+        }
+
+        // use grade value to determine grade later and display it back to the user
+        if (userGradeNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (userGradeNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (userGradeNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (userGradeNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // get letter sign of + or - (last-digit > 7 = +; last-digit < 3 = -; else no-sign)
+        int lastDigit = userGradeNum % 10;
+        if (lastDigit >= 7 && userGradeNum < 90 && userGradeNum >= 60)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && userGradeNum >= 60)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // use grade value to determine if user pass the class
+        // assuming the cutoff score is 70 at least
+        if (userGradeNum >= 70)
+        {
+            Console.WriteLine($"Congratulations! You Scored {letter}{sign}! This is a good grade.\n You pass the class.");
+        }
+        else
+        {
+            Console.WriteLine($"So sorry you scored {letter}{sign}, this is not a good grade.\n Try to work harder next time, I'm sure you'll pass if you do so.");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -12,5 +12,11 @@ class Program
         Console.Write("Are you sure?: ");
         string response = Console.ReadLine();
         Console.WriteLine($"Since you said {response}, may God bless you.");
+
+        string name = "gabriel";
+        if (!(name == "david" || name == "daniel" || name == "victor"))
+        {
+            Console.WriteLine($"{name} is not in (david, daniel, victor)");
+        }
     }
 }
