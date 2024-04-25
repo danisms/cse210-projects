840abcfe4438e729afd416fcd15d58acd7f842cd
Daniel C. Opute <danielcopute@gmail.com>
2024-04-25 17:25:25 +0100

    Completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Welcome the user
+        Console.WriteLine("Hello Welcome to Prep1!");
+
+        // Get user first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Get user last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Display user first name and last name back to the user
+        Console.WriteLine();
+        string display_name = $"Your name is {last_name}, {first_name} {last_name}.";
+        Console.WriteLine(display_name);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Hello!");
+        Console.WriteLine("Hi");
+        Console.Write("How are you?");
+        Console.WriteLine("I'm fine");
+        Console.Write("Are you sure?: ");
+        string response = Console.ReadLine();
+        Console.WriteLine($"Since you said {response}, may God bless you.");
     }
 }
