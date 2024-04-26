bfcba23bd06cf47891bf1525ead48340e48838e1
Daniel C. Opute <danielcopute@gmail.com>
2024-04-26 14:18:10 +0100

    Completed Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Function to display welcome message
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        // Function get and return user's  name
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+
+        // Function to get and return user's favorite number
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favoriteNum = int.Parse(Console.ReadLine());
+            return favoriteNum;
+        }
+
+        // Function to square number
+        static int SquareNumber(int number)
+        {
+            int squareNum = number * number;
+            return squareNum;
+        }
+
+        // Function to display result to the user
+        static void DisplayResult(string username, int number)
+        {
+            Console.WriteLine($"{username}, the square of your number {number}");
+        }
+
+        // use functions to get and return value to the user
+        // step 1: welcome the user
+        DisplayWelcome();
+        // step 2: get username
+        string username = PromptUserName();
+        // step 3: get user favorite number
+        int favoriteNum = PromptUserNumber();
+        // step 4: square user favorite number
+        int squaredNum = SquareNumber(favoriteNum);
+        // step 5: display result to the user
+        DisplayResult(username, squaredNum);
     }
 }
