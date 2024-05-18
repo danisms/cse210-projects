2b01338b1baa74af991b5c89dac3889754692b4f
Daniel C. Opute <danielcopute@gmail.com>
2024-05-18 06:53:38 +0100

    in progress

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -15,21 +15,49 @@ public class Activity
 
     public void DisplayStatingMessage()
     {
-        
+        Console.WriteLine($"Welcome to the {_name} Activity\n");
+        Console.WriteLine(_description);
     }
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine("Well done!\n");
+        if (_duration > 1)
+        {
+            Console.WriteLine($"You have completed another {_duration} second of the Breathing Activity");
+        }
+        else
+        {
+            Console.WriteLine($"You have completed another {_duration} seconds of the Breathing Activity");
+        }
     }
 
     public void ShowSpinner(int seconds)
     {
-
+        int timeInMs = seconds * 1000;
+        int speed = 500;
+        while (timeInMs > 0)
+        {
+            Console.Write('+');
+            Thread.Sleep(speed);
+            Console.Write("\b \b");  // for erasing the + character
+            Console.Write("-");  // for replacing the the position of the + character with a minus (-) character
+            timeInMs -= speed;            
+        }
     }
 
     public void ShowCountDown(int seconds)
     {
-
+        int timeInMs = seconds * 1000;
+        int count = seconds;
+        int speed = 1000;
+        while (timeInMs > 0)
+        {
+            Console.Write(count);
+            Thread.Sleep(speed);
+            Console.Write("\b \b");  // for erasing the + character
+            timeInMs -= speed;
+            Console.Write(count);  // for replacing the the position of the + character with a minus (-) character           
+        }
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        Activity newActivty = new Activity("Breathing", "Texting if it will work", 30);
+
+        newActivty.ShowSpinner(5);
     }
 }
