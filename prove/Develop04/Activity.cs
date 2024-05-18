2bc2a71a56c392ce804077e8de17bf30a0f7dadb
Daniel C. Opute <danielcopute@gmail.com>
2024-05-18 06:04:33 +0100

    mindfulness program in progress.

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography;
+
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void DisplayStatingMessage()
+    {
+        
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+
+    }
+}
---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,27 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>();
+    
+    public ListingActivity(string name, string description, int duration, int count, List<string> prompts) : base(name, description, duration)
+    {
+        _count = count;
+        _prompts = prompts;
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        List<string> list = new();
+        return list;
+    }
+}
