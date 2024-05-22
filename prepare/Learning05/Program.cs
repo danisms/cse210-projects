a758b65f5c4445a0557cfdf1fd299cde40df772b
Daniel C. Opute <danielcopute@gmail.com>
2024-05-22 13:29:16 +0100

    completed week 5 learning activity of polymophism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Hello! Welcome To Learning05!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 20));
+        shapes.Add(new Rectangle("blue", 10, 15));
+        shapes.Add(new Circle("yellow", 15));
+
+        foreach(Shape shape in shapes)
+        {
+            // get color and area of the shape
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            // display the result
+            Console.WriteLine($"Shape Color: {color}; Shape Area: {area}");
+        }
     }
 }
